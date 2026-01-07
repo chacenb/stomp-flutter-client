1b0e7275a07eb8f8048347348908032942bd6521
chace <b.tchassem@sahelys.com>
2026-01-07 07:12:09 +0100

    stomp flutter client implemented successfully OK

---
+++ lib/constants.dart
@@ -0,0 +1,2 @@
+const String localhost = 'http://10.0.2.2';
+const webSocketUrl = '$localhost:8081/socket';
---
+++ lib/main.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import 'package:stomp_dart_client/stomp.dart';
+import 'package:stomp_dart_client/stomp_config.dart';
+import 'package:stomp_dart_client/stomp_frame.dart';
+
+import 'constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  StompClient? stompClient;
+  String receivedMessage = "No messages yet";
+
+  void _incrementCounter_thenConnectToWebSocket() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _connectToWebSocket() {
+    stompClient = StompClient(
+      config: StompConfig.SockJS(
+        url: webSocketUrl,
+        onConnect: onConnectCallback,
+        beforeConnect: beforeConnectToWebSocket,
+        connectionTimeout: Duration(seconds: 10),
+        reconnectDelay: Duration(seconds: 0),
+        onStompError: onStompErrorCallback,
+        onDisconnect: onDisconnectCallback,
+        onWebSocketError: onWebSocketErrorCallback
+      ),
+    );
+    stompClient!.activate();
+  }
+
+  void disconnect() {
+    stompClient?.deactivate();
+    setState(() {
+      receivedMessage = "Disconnected";
+    });
+  }
+
+  Future<void> beforeConnectToWebSocket() async {
+    print('_____Connecting to webSocket at url $webSocketUrl');
+    setState(() {
+      receivedMessage = "Connecting to $webSocketUrl ...";
+    });
+  }
+
+  void onStompErrorCallback(StompFrame frame) {
+    print("_______ StompErrorCallback");
+    setState(() {
+      receivedMessage = frame.body ?? "StompErrorCallback";
+    });
+  }
+
+  void onDisconnectCallback(StompFrame frame) {
+    print("_______ DisconnectCallback");
+    setState(() {
+      receivedMessage = frame.body ?? "DisconnectCallback";
+    });
+  }
+
+  void onWebSocketErrorCallback(dynamic error) {
+    print("_______ WebSocketErrorCallback caused by $error");
+    setState(() {
+      receivedMessage = error.toString() ?? "Error when connecting to websocket";
+    });
+  }
+
+  void onConnectCallback(StompFrame frame) {
+    print('_________Connected!');
+    stompClient!.subscribe(
+      destination: '/topic/messages',
+      callback: (frame) {
+        setState(() {
+          receivedMessage = frame.body ?? "Empty message";
+        });
+      },
+    );
+    // Send a test message once connected
+    stompClient!.send(
+      destination: '/app/chat',
+      body: '{"user":"flutter","message":"Hello from Flutter!"}',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(receivedMessage),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _connectToWebSocket,
+                    child: Text("Connect to websocket"),
+                  ),
+                  ElevatedButton(
+                    onPressed: disconnect,
+                    child: Text("Disconnect from websocket"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter_thenConnectToWebSocket,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
